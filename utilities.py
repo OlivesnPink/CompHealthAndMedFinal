d286d85329ec172ee6a9dccfb920ac51011baec4
PixelRican <rimercado02@gmail.com>
2026-04-18 12:44:28 -0400

    Update ModelTrainer and TrainingResults
    - Simplify public interface of ModelTrainer class by exposing just the train method.
    - Add multilabel classification logic to training and validation loops.
    - Rename TrainingResults plot methods for clarity.

--- utilities.py
+++ utilities.py
@@ -1,31 +1,14 @@
 import matplotlib.pyplot as plt
 import torch
 
-def get_accuracy(predicted, labels):
-    '''
-    Returns a tuple containing the number of elements within the specified
-    tensor of ground truth labels and the number of elements within the
-    specified tensor of predicted labels that match the ground truth labels.
-
-    :param predicted: The tensor containing ground truth labels.
-    :param labels: The tensor containing predicted labels.
-    :returns: A tuple containing the number of elements within the specified
-              tensor of ground truth labels and the number of elements within
-              the specified tensor of predicted labels that match ground truth
-              labels.
-    '''
-    batch_length = labels.size(0)
-    correct_count = (predicted == labels).sum().item()
-    return batch_length, correct_count
-
 class ModelTrainer:
     '''
     Provides methods that train and validate data models according to a set of
     configurations.
     '''
     def __init__(self,
-                 training_data_loader,
-                 validation_data_loader,
+                 training_set,
+                 validation_set,
                  loss_criterion,
                  optimizer,
                  device):
@@ -35,47 +18,17 @@ class ModelTrainer:
         device using specified loss criterion and optimizer.
 
         :param self: The instance to initialize.
-        :param training_data_loader: The data loader containing the training
-                                     set.
-        :param validation_data_loader: The data loader containing the validation
-                                       set.
+        :param training_set: The data loader containing the training set.
+        :param validation_set: The data loader containing the validation set.
         :param loss_criterion: The loss function to use during training.
         :param optimizer: The optimizer to use during training.
         :param device: The device to load data into.
         '''
-        self.training_data_loader = training_data_loader
-        self.validation_data_loader = validation_data_loader
+        self.training_set = training_set
+        self.validation_set = validation_set
         self.loss_criterion = loss_criterion
         self.optimizer = optimizer
         self.device = device
-
-    def validate(self, model):
-        '''
-        Validates the specified data model on the validation set contained by
-        the ModelTrainer.
-
-        :param self: The ModelTrainer.
-        :param model: The data model to validate.
-        :return: A tuple containing validation accuracy and loss metrics.
-        '''
-        loss = 0
-        sample_total = 0
-        correct_total = 0
-        model.eval()
-
-        for inputs, labels in self.validation_data_loader:
-            inputs, labels = inputs.to(self.device), labels.to(self.device)
-            outputs = model(inputs)
-            # TODO: Change to multilabel classification.
-            _, predicted = torch.max(outputs, 1)
-            loss += self.loss_criterion(outputs, labels).item()
-            batch_length, correct_count = get_accuracy(predicted, labels)
-            sample_total += batch_length
-            correct_total += correct_count
-
-        accuracy = correct_total / sample_total
-        loss /= len(self.validation_data_loader)
-        return accuracy, loss
 
     def train(self, model, epoch_count):
         '''
@@ -93,27 +46,43 @@ class ModelTrainer:
         validation_losses = [0] * epoch_count
 
         for epoch in range(epoch_count):
-            training_loss = 0
+            # Begin training loop.
+            loss_total = 0
             sample_total = 0
             correct_total = 0
 
-            for inputs, labels in self.training_data_loader:
+            for inputs, labels in self.training_set:
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
                 self.optimizer.zero_grad()
                 outputs = model(inputs)
-                loss_tensor = self.loss_criterion(outputs, labels)
-                loss_tensor.backward()
+                predictions = outputs > 0.5
+                losses = self.loss_criterion(outputs, labels)
+                losses.backward()
                 self.optimizer.step()
-                # TODO: Change to multilabel classification.
-                _, predicted = torch.max(outputs, 1)
-                batch_length, correct_count = get_accuracy(predicted, labels)
-                training_loss += loss_tensor.item()
-                sample_total += batch_length
-                correct_total += correct_count
+                loss_total += losses.item()
+                sample_total += labels.size(0)
+                correct_total += (predictions == labels).sum().item()
 
             training_accuracies[epoch] = correct_total / sample_total
-            training_losses[epoch] = training_loss / len(self.training_data_loader)
-            validation_accuracies[epoch], validation_losses[epoch] = self.validate(model)
+            training_losses[epoch] = loss_total / len(self.training_set)
+
+            # Begin validation loop.
+            loss_total = 0
+            sample_total = 0
+            correct_total = 0
+            model.eval()
+
+            for inputs, labels in self.validation_set:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = model(inputs)
+                predictions = outputs > 0.5
+                losses = self.loss_criterion(outputs, labels)
+                loss_total += losses.item()
+                sample_total += labels.size(0)
+                correct_total += (predictions == labels).sum().item()
+
+            validation_accuracies[epoch] = correct_total / sample_total
+            validation_losses[epoch] = loss_total / len(self.validation_set)
 
         return TrainingResults(training_accuracies,
                                training_losses,
@@ -145,7 +114,7 @@ class TrainingResults:
         self.validation_accuracies = validation_accuracies
         self.validation_losses = validation_losses
 
-    def plot_accuracies(self):
+    def show_accuracies(self):
         epochs = range(1, len(self.training_accuracies) + 1)
         plt.figure(figsize=(10, 15))
         plt.subplot(2, 1, 2)
@@ -157,7 +126,7 @@ class TrainingResults:
         plt.figure()
         plt.show()
 
-    def plot_losses(self):
+    def show_losses(self):
         epochs = range(1, len(self.training_losses) + 1)
         plt.figure(figsize=(10, 15))
         plt.subplot(2, 1, 2)
